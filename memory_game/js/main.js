a8ce5ff208d23fe49653217d1da041ec95c924e1
leanaAlq <leana.a.q@gmail.com>
2018-09-27 18:23:10 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+console.log("Up and running!");
+var card1 = "queen";
+var card2 = "king";
+var card3 = "king";
+var card4 = "queen";
+console.log("user flipped "+ card1);
+console.log("user flipped "+ card2);

74213c3196930c86da04e41345cddd20718c2101
leanaAlq <leana.a.q@gmail.com>
2018-09-29 13:04:29 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,19 @@
 
 console.log("Up and running!");
+var cards =["queen","king","queen","king"];
 var cardsInPlay = [];
-var cards =["queen","queen","king","king"];
-alert('Hello, friends.');
-var cardOne = cards[0];
-var cardTwo = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("user flipped "+ cardsInPlay[0]);
-console.log("user flipped "+ cardsInPlay[1]);
-if(cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1])
-alert("you found a match");
-		else 
-alert("sorry try again!");
+var checkForMatch= function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+
+}}
+var flipCard = function (cardId){
+console.log("user flipped "+ cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 }
+flipCard(0);
+flipCard(1);
+checkForMatch();
 
